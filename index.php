7ebe3cdcebae73718e70b25e0a6e9b43f30cd713
hectorJULIUS <hectorjulius51@gmail.com>
2021-11-22 12:42:30 +0300

    assets,links

--- index.php
+++ index.php
@@ -5,8 +5,29 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Legends</title>
+    <!--google fonts-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+
+    <!--font awsome-->
+    <script src="https://kit.fontawesome.com/1f8378c25b.js" crossorigin="anonymous"></script>
+
+    
+
+    <!---bootsrap css-->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <!--custome css-->
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans+Condensed:ital,wght@0,300;0,700;1,300&display=swap" rel="stylesheet">
+
 </head>
 <body>
     
+<script src="assets/js/jquery-3.6.0.min.js"></script>
+<script src="assets/js/bootstrap.bundle.min.js"></script>
 </body>
+
 </html>
