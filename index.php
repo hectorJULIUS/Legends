c86750ef10ce840996520929d4651f8af07fface
hectorJULIUS <hectorjulius51@gmail.com>
2021-11-24 12:22:27 +0300

    index updates, styles updates

--- index.php
+++ index.php
@@ -175,5 +175,96 @@
 
     </section>
 
+    <!--News section-->
+    <section class="newsSection bg-light">
+        <center>
+            <h2 class="text-center">Latest News</h2>
+            <div class="border"></div>
+            <div class="row newsSectionRow mt-5">
+                <div class="col-md-4">
+                    <div class="card newsCard">
+                        <div class="card-header">
+                            <img src="images/maxime-agnelli-6neOO6DmkiY-unsplash.jpg" height="300px" width="100%">
+                        </div>
+
+                        <div class="card-body">
+                            <span id="newsDate">17th Jul 2021</span> &nbsp;
+                            <span class="poster">By Muoka</span>
+                            <hr>
+                            <h5 class="py-3">
+                                <strong>Rally Camping</strong>
+                            </h5>
+                            <p>
+                              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever
+                            </p>
+                        </div>
+
+                    </div>
+
+                </div>
+                <!--end of column1-->
+                
+                <div class="col-md-4">
+                  <div class="card newsCard">
+                      <div class="card-header">
+                          <img src="images/george-brynzan-x5M3IFKmdxk-unsplash.jpg" height="300px" width="100%">
+                      </div>
+
+                      <div class="card-body">
+                          <span id="newsDate">10th Aug 2021</span> &nbsp;
+                          <span class="poster">By Julius</span>
+                          <hr>
+                          <h5 class="py-3">
+                              <strong>Streets Benefits</strong>
+                          </h5>
+                          <p>
+                              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever
+                          </p>
+                      </div>
+
+                  </div>
+
+              </div>
+              <div class="col-md-4">
+                  <div class="card newsCard">
+                      <div class="card-header">
+                          <img src="images/nick-fewings-RgJqQPZupgM-unsplash.jpg" height="300px" width="100%">
+                      </div>
+
+                      <div class="card-body">
+                          <span id="newsDate">26th Sept 2021</span> &nbsp;
+                          <span class="poster">By Hector</span>
+                          <hr>
+                          <h5 class="py-3">
+                              <strong>Race Track competitions</strong>
+                          </h5>
+                          <p>
+                              Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever
+                              
+                          </p>
+                      </div>
+
+                  </div>
+
+              </div>
+            </div>
+            <a href="#">
+              <button style="border-radius: 40px;
+              margin: 40px;
+              width: 14%;
+              padding: 10px;
+              color: #fff;
+              font-family:calibri;
+              transition: 0.5s linear;
+              background-color:  #0c10fe;" class="btn btn-lg btn-default site_btn">
+                  <i class="fa fa-eye"></i> &nbsp;
+                  View More
+              </button>
+          </a>
+        </center>
+
+    </section>
+    <!--End of News section-->
+
     <?php include 'footer.php' ;?>
     
